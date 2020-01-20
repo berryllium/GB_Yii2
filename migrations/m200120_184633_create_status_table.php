db7b2a91f4e8b6c1f049a5f976563c5bec0ffcc1
berryllium <gorkundp@yandex.ru>
2020-01-21 00:08:32 +0300

    task 1

---
+++ migrations/m200120_184633_create_status_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%status}}`.
+ */
+class m200120_184633_create_status_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%status}}', [
+            'id' => $this->primaryKey(),
+            'status' => $this->string()->notNull(),
+            'description' => $this->string()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%status}}');
+    }
+}
---
+++ models/tables/Users.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string|null $login
+ * @property string $name
+ * @property string|null $password
+ * @property string $role
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'role'], 'required'],
+            [['login', 'name', 'role'], 'string', 'max' => 255],
+            [['password'], 'string', 'max' => 32],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'login' => 'Login',
+            'name' => 'Name',
+            'password' => 'Password',
+            'role' => 'Role',
+        ];
+    }
+}
