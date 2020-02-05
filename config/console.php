9cfe3c42bd770ac4e143330407c8cf443e6b7933
berryllium <gorkundp@yandex.ru>
2020-02-05 22:23:45 +0300

    рассылка о дедлайне

--- config/console.php
+++ config/console.php
@@ -14,6 +14,13 @@ $config = [
         '@tests' => '@app/tests',
     ],
     'components' => [
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
